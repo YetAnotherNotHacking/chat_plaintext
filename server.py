e47fc58599133dabef1c1cdef10fccb57f1872cd
NotHacking <yetanotherdingus@gmail.com>
2024-01-17 09:23:09 +0100

    Changed code or smth
    why you reading me

--- server.py
+++ server.py
@@ -1,100 +1,106 @@
-import socket
-import threading
+import socket, threading
 
-class Server:
-    def __init__(self, host='127.0.0.1', port=45685):
-        self.host = host
-        self.port = port
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.host, self.port))
-        self.server.listen()
-        self.clients = []
-        self.nicknames = []
-        self.channels = []
-        self.lock = threading.Lock()
+# Global variable that mantain client's connections
+connections = []
 
-    def broadcast(self, message, sender):
-        with self.lock:
-            for client in self.clients:
-                if client != sender:
-                    try:
-                        client.sendall(message)
-                    except Exception as e:
-                        print("Error in the broadcast function, error:", e)
+def handle_user_connection(connection: socket.socket, address: str) -> None:
+    '''
+        Get user connection in order to keep receiving their messages and
+        sent to others users/connections.
+    '''
+    while True:
+        try:
+            # Get client message
+            msg = connection.recv(1024)
 
-    def handle(self, client, nickname):
-        while True:
-            try:
-                message = client.recv(1024)
-                self.process_message(message, client, nickname)
-            except:
-                with self.lock:
-                    index = self.clients.index(client)
-                    self.clients.remove(client)
-                    client.close()
-                    nickname = self.nicknames[index]
-                    self.nicknames.remove(nickname)
-                    self.broadcast(f'{nickname} left the chat!'.encode('ascii'), client)
+            # If no message is received, there is a chance that connection has ended
+            # so in this case, we need to close connection and remove it from connections list.
+            if msg:
+                # Log message sent by user
+                print(f'{address[0]}:{address[1]} - {msg.decode()}')
+                
+                # Build message format and broadcast to users connected on server
+                msg_to_send = f'From {address[0]}:{address[1]} - {msg.decode()}'
+                broadcast(msg_to_send, connection)
+
+            # Close connection if no message was sent
+            else:
+                remove_connection(connection)
                 break
 
-    def process_message(self, message, client, nickname):
-        command = message.decode('ascii').strip().split(':')
-        if command[0] == 'LIST_CHANNELS':
-            self.send_channel_list(client)
-        elif command[0] == 'CREATE_CHANNEL':
-            self.create_channel(command[1])
-        elif command[0] == 'DELETE_CHANNEL':
-            self.remove_channel(command[1])
-        else:
-            self.broadcast(f'{nickname}: {message}', client)
+        except Exception as e:
+            print(f'Error to handle user connection: {e}')
+            remove_connection(connection)
+            break
 
-    def create_channel(self, channel_name):
-        with self.lock:
-            if channel_name not in self.channels:
-                self.channels.append(channel_name)
-                print(f'Channel "{channel_name}" created.')
 
-    def list_channels(self, client):
-        with self.lock:
-            return self.channels.copy()
+def broadcast(message: str, connection: socket.socket) -> None:
+    '''
+        Broadcast message to all users connected to the server
+    '''
 
-    def send_channel_list(self, client):
-        channels = ','.join(self.list_channels(client))
-        client.send(f'LIST_CHANNELS:{channels}'.encode('ascii'))
-
-    def remove_channel(self, channel_name):
-        with self.lock:
-            if channel_name in self.channels:
-                self.channels.remove(channel_name)
-                print(f'Channel "{channel_name}" removed.')
-
-    def receive(self):
-        print('Waiting for connections...')
-        while True:
+    # Iterate on connections in order to send message to all client's connected
+    for client_conn in connections:
+        # Check if isn't the connection of who's send
+        if client_conn != connection:
             try:
-                client, address = self.server.accept()
-                print(f'Connected with {str(address)}')
+                # Sending message to client connection
+                client_conn.send(message.encode())
 
-                client.send('NICK'.encode('ascii'))
-                nickname = client.recv(1024).decode('ascii')
-
-                with self.lock:
-                    self.nicknames.append(nickname)
-                    self.clients.append(client)
-
-                print(f'Nickname of the client is {nickname}!')
-                self.broadcast(f'{nickname} joined the chat!'.encode('ascii'), client)
-                client.send('Connected to the server!'.encode('ascii'))
-
-                thread = threading.Thread(target=self.handle, args=(client, nickname))
-                thread.start()
+            # if it fails, there is a chance of socket has died
             except Exception as e:
-                print(f"Error accepting connection: {e}")
+                print('Error broadcasting message: {e}')
+                remove_connection(client_conn)
 
-    def start(self):
-        print('Server started...')
-        self.receive()
 
-# Instantiate the server and start it
-server = Server()
-server.start()
+def remove_connection(conn: socket.socket) -> None:
+    '''
+        Remove specified connection from connections list
+    '''
+
+    # Check if connection exists on connections list
+    if conn in connections:
+        # Close socket connection and remove connection from connections list
+        conn.close()
+        connections.remove(conn)
+
+
+def server() -> None:
+    '''
+        Main process that receive client's connections and start a new thread
+        to handle their messages
+    '''
+
+    LISTENING_PORT = 12000
+    
+    try:
+        # Create server and specifying that it can only handle 4 connections by time!
+        socket_instance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_instance.bind(('', LISTENING_PORT))
+        socket_instance.listen(4)
+
+        print('Server running!')
+        
+        while True:
+
+            # Accept client connection
+            socket_connection, address = socket_instance.accept()
+            # Add client connection to connections list
+            connections.append(socket_connection)
+            # Start a new thread to handle client connection and receive it's messages
+            # in order to send to others connections
+            threading.Thread(target=handle_user_connection, args=[socket_connection, address]).start()
+
+    except Exception as e:
+        print(f'An error has occurred when instancing socket: {e}')
+    finally:
+        # In case of any problem we clean all connections and close the server connection
+        if len(connections) > 0:
+            for conn in connections:
+                remove_connection(conn)
+
+        socket_instance.close()
+
+
+if __name__ == "__main__":
+    server()
