3b0ad6b36f93b0f62754b047f69ebee34772a8b5
NotHacking <yetanotherdingus@gmail.com>
2024-01-17 09:22:35 +0100

    Used other code
    boo

--- client.py
+++ client.py
@@ -1,122 +1,63 @@
-import socket
-import threading
-import PySimpleGUI as sg
+import socket, threading
 
-class Client:
-    def __init__(self, host='127.0.0.1', port=45685):
-        self.nickname = ''
-        self.channel = ''
-        self.channels = []
+def handle_messages(connection: socket.socket):
+    '''
+        Receive messages sent by the server and display them to user
+    '''
 
-        # Get nickname from user
-        self.get_nickname()
+    while True:
+        try:
+            msg = connection.recv(1024)
 
-        # Set up the GUI layout
-        self.layout = [
-            [sg.Text(f'Nickname: {self.nickname}', key='-NICKNAME-', size=(20, 1)),
-             sg.Button('Change Nickname')],
-            [sg.Listbox(values=self.channels, size=(20, 10), key='-CHANNELS-'),
-             sg.Multiline(size=(60, 20), key='-MESSAGES-')],
-            [sg.Input(key='-MESSAGE-', size=(40, 1)), sg.Button('Send'), sg.Button('Exit')],
-            [sg.Button('Create Channel'), sg.Button('Delete Channel')]
-        ]
-
-        self.window = sg.Window('Chat Platform', self.layout, return_keyboard_events=True)
-
-        # Connect to the server
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client.connect((host, port))
-
-        # Send the nickname to the server
-        self.client.send('NICK'.encode('ascii'))
-        self.client.send(self.nickname.encode('ascii'))
-
-        # Request the list of channels from the server
-        self.client.send('LIST_CHANNELS'.encode('ascii'))
-
-    def get_nickname(self):
-        while True:
-            self.nickname = sg.popup_get_text('Enter your nickname:')
-            if self.nickname:
+            # If there is no message, there is a chance that connection has closed
+            # so the connection will be closed and an error will be displayed.
+            # If not, it will try to decode message in order to show to user.
+            if msg:
+                print(msg.decode())
+            else:
+                connection.close()
                 break
 
-    def receive(self):
+        except Exception as e:
+            print(f'Error handling message from server: {e}')
+            connection.close()
+            break
+
+def client() -> None:
+    '''
+        Main process that start client connection to the server 
+        and handle it's input messages
+    '''
+
+    SERVER_ADDRESS = '127.0.0.1'
+    SERVER_PORT = 12000
+
+    try:
+        # Instantiate socket and start connection with server
+        socket_instance = socket.socket()
+        socket_instance.connect((SERVER_ADDRESS, SERVER_PORT))
+        # Create a thread in order to handle messages sent by server
+        threading.Thread(target=handle_messages, args=[socket_instance]).start()
+
+        print('Connected to chat!')
+
+        # Read user's input until it quit from chat and close connection
         while True:
-            try:
-                message = self.client.recv(1024).decode('ascii')
-                self.process_message(message)
-            except:
-                print("An error occurred!")
-                self.client.close()
+            msg = input()
+
+            if msg == 'quit':
                 break
 
-    def process_message(self, message):
-        command = message.strip().split(':')
-        if command[0] == 'LIST_CHANNELS':
-            self.update_channel_list(command[1])
-        else:
-            print(message)
+            # Parse message to utf-8
+            socket_instance.send(msg.encode())
 
-    def update_channel_list(self, channels):
-        channel_list = channels.split(',')
-        self.window['-CHANNELS-'].update(values=channel_list)
+        # Close connection with the server
+        socket_instance.close()
 
-    def write(self, message):
-        full_message = f'{self.channel}: {message}'
-        self.client.send(full_message.encode('ascii'))
+    except Exception as e:
+        print(f'Error connecting to server socket {e}')
+        socket_instance.close()
 
-    def list_channels(self):
-        self.client.send('LIST_CHANNELS'.encode('ascii'))
 
-    def create_channel(self, channel_name):
-        self.client.send(f'CREATE_CHANNEL:{channel_name}'.encode('ascii'))
-
-    def delete_channel(self, channel_name):
-        self.client.send(f'DELETE_CHANNEL:{channel_name}'.encode('ascii'))
-
-    def start(self):
-        receive_thread = threading.Thread(target=self.receive)
-        receive_thread.start()
-
-        while True:
-            event, values = self.window.read()
-
-            if event in (sg.WIN_CLOSED, 'Exit'):
-                self.client.close()
-                break
-
-            if event == 'Send' or (event == '\r' and values['-MESSAGE-']):
-                message = values["-MESSAGE-"].strip()
-                if message:
-                    self.write(message)
-                    self.window['-MESSAGE-'].update('')  # Clear the input field
-
-            if event == 'Change Nickname':
-                self.get_nickname()
-                self.client.send(f'NICK:{self.nickname}'.encode('ascii'))
-
-            if event == '-CHANNELS-':
-                selected_channel = values['-CHANNELS-'][0]
-                if selected_channel != self.channel:
-                    self.channel = selected_channel
-                    sg.popup(f'Switched to channel: {self.channel}')
-
-            if event == 'LIST_CHANNELS':
-                self.list_channels()
-
-            if event == 'Create Channel':
-                channel_name = sg.popup_get_text('Enter the channel name:')
-                if channel_name:
-                    self.create_channel(channel_name)
-
-            if event == 'Delete Channel':
-                channel_name = sg.popup_get_text('Enter the channel name to delete:')
-                if channel_name:
-                    self.delete_channel(channel_name)
-
-# Create the client instance
-client = Client()
-client.start()
-
-# Close the PySimpleGUI window
-client.window.close()
+if __name__ == "__main__":
+    client()
